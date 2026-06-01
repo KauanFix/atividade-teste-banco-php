012de78686072ef218e0ccce94136a270c58fd76
KauanFix <kauan_fix@estudante.sesisenai.org.br>
2026-06-01 09:31:28 -0300

    style - comentários explicando a criação de tabelas, inserção de dados e mostrar na tela

--- public/component/table.php
+++ public/component/table.php
@@ -1,29 +1,29 @@
 <hr>
 
-<h4> Usuários Cadastrados </h4>
+<h4> Usuários Cadastrados </h4> 
 
-<table border="1" cellpadding="10">
+<table border="1" cellpadding="10"> <!-- cria a tabela -->
 
-    <tr>
+    <tr> <!-- insere uma linha -->
         <th>ID</th>
-        <th>Usuários</th>
+        <th>Usuários</th> <!-- insere os cabeçalhos -->
         <th>Senha</th>
     </tr>
 
     <?php
     
-    $sqlUsuarios = "SELECT * FROM usuario";
+    $sqlUsuarios = "SELECT * FROM usuario"; //variável para pegar todos os dados de "usuario"
 
-    $resultadoUsuario = $conn -> query($sqlUsuarios);
+    $resultadoUsuario = $conn -> query($sqlUsuarios); //guardar os dados do banco
 
-    while($linha = $resultadoUsuario -> fetch_assoc()){
+    while($linha = $resultadoUsuario -> fetch_assoc()){ //enquanto a variável linha for igual ao resultado, ele cria uma nova linha na tabela, fetch_assoc() transforma num array associativo para o php poder ler
         echo"<tr>
         
         <td>".$linha["id"]."</td>
         <td>".$linha["nome"]."</td>
         <td>".$linha["senha"]."</td>
 
-        </tr>";
+        </tr>"; //cria uma nova linha na tabela com os dados do banco
     }
     
     
